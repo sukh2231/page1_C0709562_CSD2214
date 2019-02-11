e91b88ad463e4265a740e8194b23734286b95be6
visualstudio <visualstudio@230-PC30>
2019-02-11 16:57:09 -0500

    worksheetnumber1

--- page1_C0709562_CSD2214/js/main.js
+++ page1_C0709562_CSD2214/js/main.js
@@ -1,1 +1,33 @@
-﻿// Your code here!
+﻿<html>
+<titlle>Event!!!</titlle>
+<script type="text/javascript">
+    fuction trigger()
+    {
+        document.getElementByld("hover").addEventListener("moouseover", popup);
+
+        fuction popup()
+
+        {
+            alert("Welcome to my webPage!!!");
+
+        }
+    }
+</script>
+<style>
+
+    p
+    
+    {
+        font-size:50px;
+        position: fixed:
+            left:550px;
+        top:300px;
+
+    }
+
+</style>
+</head>
+<body onload="trigger();"">
+    <p id="hover">Welcome!!!</p>
+</body>
+</html>
